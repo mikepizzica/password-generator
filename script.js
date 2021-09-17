a26eaee225d3cb5dcdb1e80bbfa705301612bee2
Mike Pizzica <mike.pizzica@gmail.com>
2021-09-17 15:07:08 -0400

    added decomposition and notes in comments for all acceptance criteria

--- script.js
+++ script.js
@@ -13,27 +13,36 @@ function writePassword() {
 // GIVEN I need a new, secure password
 // WHEN I click the button to generate a password
 // THEN I am presented with a series of prompts for password criteria
+// tie button click to prompts for criteria
+
 // WHEN prompted for password criteria
 // THEN I select which criteria to include in the password
+// checkboxes for prompts?
+
 // WHEN prompted for the length of the password
 // THEN I choose a length of at least 8 characters and no more than 128 characters
 // if length of password is >= 8 and <=128
 // the pasword is valid
 // else
 // the password is invalid
+
 // WHEN asked for character types to include in the password
 // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
 // var lowercase = true/false???
 // var uppercase = true/false???
 // var numeric = true/false???
 // var special = ture/false???
+
 // WHEN I answer each prompt
 // THEN my input should be validated and at least one character type should be selected
+// need to research "validated" and "character type"
+
 // WHEN all prompts are answered
 // THEN a password is generated that matches the selected criteria
+// cross-reference generated password w/ criteria selected earlier
+
 // WHEN the password is generated
 // THEN the password is either displayed in an alert or written to the page
 // alert("Password is: ....");
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
