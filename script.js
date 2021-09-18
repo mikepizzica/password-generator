0e709c6464cf2cc3d5c807eb174f7b3d33047049
Mike Pizzica <mike.pizzica@gmail.com>
2021-09-18 13:37:27 -0400

    added more ideas for execution in comments of js

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ function writePassword() {
 // GIVEN I need a new, secure password
 // WHEN I click the button to generate a password
 // THEN I am presented with a series of prompts for password criteria
-// tie button click to prompts for criteria
+// tie button click to prompts for criteria.
 
 // WHEN prompted for password criteria
 // THEN I select which criteria to include in the password
-// checkboxes for prompts?
+// checkboxes for prompts? t/f? tie responses to if statements
 
 // WHEN prompted for the length of the password
 // THEN I choose a length of at least 8 characters and no more than 128 characters
@@ -39,7 +39,7 @@ function writePassword() {
 
 // WHEN all prompts are answered
 // THEN a password is generated that matches the selected criteria
-// cross-reference generated password w/ criteria selected earlier
+// cross-reference generated password w/ criteria selected earlier. use a function here?
 
 // WHEN the password is generated
 // THEN the password is either displayed in an alert or written to the page
