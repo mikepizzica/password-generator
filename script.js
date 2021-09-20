9c53d511478e083fd609d7d220049ca563d517c4
Mike Pizzica <mike.pizzica@gmail.com>
2021-09-19 20:42:48 -0400

    added empty prompts

--- script.js
+++ script.js
@@ -14,10 +14,12 @@ function writePassword() {
 // WHEN I click the button to generate a password
 // THEN I am presented with a series of prompts for password criteria
 // tie button click to prompts for criteria.
+var userGuess = prompt("");
 
 // WHEN prompted for password criteria
 // THEN I select which criteria to include in the password
 // checkboxes for prompts? t/f? tie responses to if statements
+var userGuess = prompt("");
 
 // WHEN prompted for the length of the password
 // THEN I choose a length of at least 8 characters and no more than 128 characters
@@ -25,6 +27,7 @@ function writePassword() {
 // the pasword is valid
 // else
 // the password is invalid
+var userGuess = prompt("");
 
 // WHEN asked for character types to include in the password
 // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
