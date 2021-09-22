8531768e59093e677783ba09bd875a07d9a61539
Mike Pizzica <mike.pizzica@gmail.com>
2021-09-22 09:23:01 -0400

    revise code to remove unnecessary lines. add comments for full context. add 'return' to 'getLength()' function to cover cases where 'else' portion of that function is hit.

--- script.js
+++ script.js
@@ -1,130 +1,16 @@
-// // TODO come back to this, uncomment here thru line 12, and tie to button click
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-
-  // passwordText.value = password;
-
-// }
-
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-function writePassword() {
-
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-  function getLength() {
-    var passwordLength = prompt("Enter desired character length of password (8 to 128)");
-    if (8 <= passwordLength && passwordLength <= 128) {
-      console.log(passwordLength);
-      return passwordLength;
-    } else {
-      alert("Please Enter Valid Criteria");
-      getLength();
-    }
-  }
-  var passwordLength = getLength();
-
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-  var includeLowercase = confirm("Include Lowercase?");
-  console.log(includeLowercase);
-  var includeUppercase = confirm("Include Uppercase?");
-  console.log(includeUppercase);
-  var includeNumeric = confirm("Include Numeric?");
-  console.log(includeNumeric);
-  var includeSpecial = confirm("Include Special Characters?");
-  console.log(includeSpecial);
-  var result = '';
-
-  function generatePassword() {
-    if (includeLowercase === true && includeUppercase === false && includeNumeric === false && includeSpecial === false) {
-      for (var i = 0; i < passwordLength; i++) {
-        console.log(result += Lowercase[Math.floor(Math.random() * Lowercase.length)]);
-      }
-    } else if (includeLowercase === true && includeUppercase === true && includeNumeric === false && includeSpecial === false) {
-      for (var i = 0; i < passwordLength; i++) {
-        console.log(result += LowercaseUppercase[Math.floor(Math.random() * LowercaseUppercase.length)]);
-      }
-    } else if (includeLowercase === true && includeUppercase === false && includeNumeric === true && includeSpecial === false) {
-      for (var i = 0; i < passwordLength; i++) {
-        console.log(result += LowercaseNumeric[Math.floor(Math.random() * LowercaseNumeric.length)]);
-      }
-    } else if (includeLowercase === true && includeUppercase === false && includeNumeric === false && includeSpecial === true) {
-      for (var i = 0; i < passwordLength; i++) {
-        console.log(result += LowercaseSpecial[Math.floor(Math.random() * LowercaseSpecial.length)]);
-      }
-    } else if (includeLowercase === true && includeUppercase === true && includeNumeric === true && includeSpecial === false) {
-      for (var i = 0; i < passwordLength; i++) {
-        console.log(result += LowercaseUppercaseNumeric[Math.floor(Math.random() * LowercaseUppercaseNumeric.length)]);
-      }
-    } else if (includeLowercase === true && includeUppercase === true && includeNumeric === false && includeSpecial === true) {
-      for (var i = 0; i < passwordLength; i++) {
-        console.log(result += LowercaseUppercaseSpecial[Math.floor(Math.random() * LowercaseUppercaseSpecial.length)]);
-      }
-    } else if (includeLowercase === true && includeUppercase === false && includeNumeric === true && includeSpecial === true) {
-      for (var i = 0; i < passwordLength; i++) {
-        console.log(result += LowercaseNumericSpecial[Math.floor(Math.random() * LowercaseNumericSpecial.length)]);
-      }
-    } else if (includeLowercase === true && includeUppercase === true && includeNumeric === true && includeSpecial === true) {
-      for (var i = 0; i < passwordLength; i++) {
-        console.log(result += LowercaseUppercaseNumericSpecial[Math.floor(Math.random() * LowercaseUppercaseNumericSpecial.length)]);
-      }
-    } else if (includeLowercase === false && includeUppercase === true && includeNumeric === false && includeSpecial === false) {
-      for (var i = 0; i < passwordLength; i++) {
-        console.log(result += Uppercase[Math.floor(Math.random() * Uppercase.length)]);
-      }
-    } else if (includeLowercase === false && includeUppercase === true && includeNumeric === true && includeSpecial === false) {
-      for (var i = 0; i < passwordLength; i++) {
-        console.log(result += UppercaseNumeric[Math.floor(Math.random() * UppercaseNumeric.length)]);
-      }
-    } else if (includeLowercase === false && includeUppercase === true && includeNumeric === false && includeSpecial === true) {
-      for (var i = 0; i < passwordLength; i++) {
-        console.log(result += UppercaseSpecial[Math.floor(Math.random() * UppercaseSpecial.length)]);
-      }
-    } else if (includeLowercase === false && includeUppercase === true && includeNumeric === true && includeSpecial === true) {
-      for (var i = 0; i < passwordLength; i++) {
-        console.log(result += UppercaseNumericSpecial[Math.floor(Math.random() * UppercaseNumericSpecial.length)]);
-      }
-    } else if (includeLowercase === false && includeUppercase === false && includeNumeric === true && includeSpecial === false) {
-      for (var i = 0; i < passwordLength; i++) {
-        console.log(result += Numeric[Math.floor(Math.random() * Numeric.length)]);
-      }
-    } else if (includeLowercase === false && includeUppercase === false && includeNumeric === true && includeSpecial === true) {
-      for (var i = 0; i < passwordLength; i++) {
-        console.log(result += NumericSpecial[Math.floor(Math.random() * NumericSpecial.length)]);
-      }
-    } else if (includeLowercase === false && includeUppercase === false && includeNumeric === false && includeSpecial === true) {
-      for (var i = 0; i < passwordLength; i++) {
-        console.log(result += Special[Math.floor(Math.random() * Special.length)]);
-      }
-    } else {
-      alert("Enter valid criteria")
-    }
-  }
-  generatePassword()
-  console.log(result)
-  var passwordText = document.querySelector("#password");
-  passwordText.value = result;
-}
-
-// var lowercase = true/false??? 26 characters below
+// We have four groups of possible characters 
+// 2^4 = 16, therefore there are 16 combinations total
+// One combination is "don't include any array" which will return "Please Enter Valid Criteria"
+// The 15 valid combinations are written below
+// 26 lowercase letters
 var Lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// var uppercase = true/false??? 26 characters below
+// 26 uppercase letters
 var Uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-// var numeric = true/false??? 10 characters below
+// 10 numeric digits
 var Numeric = [0,1,2,3,4,5,6,7,8,9];
-// var special = true/false??? 33 characters below
+// 33 special characters per https://www.owasp.org/index.php/Password_special_characters
 var Special = [" ","!","\"","#","$","%","&","\`","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
-// Combinations below
+// 11 combinations of two or more of the four arrays above are written below
 LowercaseUppercase = Lowercase.concat(Uppercase);
 LowercaseNumeric = Lowercase.concat(Numeric);
 LowercaseSpecial = Lowercase.concat(Special);
@@ -137,35 +23,99 @@ UppercaseSpecial = Uppercase.concat(Special);
 UppercaseNumericSpecial = Uppercase.concat(Numeric,Special);
 NumericSpecial = Numeric.concat(Special);
 
-// made a test "for" loop to check above arrays
-for (var i = 0; i < NumericSpecial.length; i++) {
-    console.log(NumericSpecial[i]);
+// function to write the password which is assigned to our button click
+function writePassword() {
+
+  // function to get input for the length of the password which must be a number between 8 and 128, inclusive
+  function getLength() {
+    var passwordLength = prompt("Enter desired character length of password (8 to 128)");
+    if (8 <= passwordLength && passwordLength <= 128) {
+      return passwordLength;
+    } else {
+      alert("Please Enter Valid Criteria");
+      return getLength();
+    }
+  }
+  // caling the function directly above and storing the value of the input for length in a variable so it can be called later
+  var passwordLength = getLength();
+
+  // prompts to get boolean t/f values for whether or not an array should be included
+  var includeLowercase = confirm("Include Lowercase?");
+  var includeUppercase = confirm("Include Uppercase?");
+  var includeNumeric = confirm("Include Numeric?");
+  var includeSpecial = confirm("Include Special Characters?");
+
+  // storing the value of the result in variable which we will use in the logic directly below
+  var result = '';
+  // logic to generate a password using all set up work in the code up to this point and user input that has been recorded
+  function generatePassword() {
+    if (includeLowercase === true && includeUppercase === false && includeNumeric === false && includeSpecial === false) {
+      for (var i = 0; i < passwordLength; i++) {
+        result += Lowercase[Math.floor(Math.random() * Lowercase.length)];
+      }
+    } else if (includeLowercase === true && includeUppercase === true && includeNumeric === false && includeSpecial === false) {
+      for (var i = 0; i < passwordLength; i++) {
+        result += LowercaseUppercase[Math.floor(Math.random() * LowercaseUppercase.length)];
+      }
+    } else if (includeLowercase === true && includeUppercase === false && includeNumeric === true && includeSpecial === false) {
+      for (var i = 0; i < passwordLength; i++) {
+        result += LowercaseNumeric[Math.floor(Math.random() * LowercaseNumeric.length)];
+      }
+    } else if (includeLowercase === true && includeUppercase === false && includeNumeric === false && includeSpecial === true) {
+      for (var i = 0; i < passwordLength; i++) {
+        result += LowercaseSpecial[Math.floor(Math.random() * LowercaseSpecial.length)];
+      }
+    } else if (includeLowercase === true && includeUppercase === true && includeNumeric === true && includeSpecial === false) {
+      for (var i = 0; i < passwordLength; i++) {
+        result += LowercaseUppercaseNumeric[Math.floor(Math.random() * LowercaseUppercaseNumeric.length)];
+      }
+    } else if (includeLowercase === true && includeUppercase === true && includeNumeric === false && includeSpecial === true) {
+      for (var i = 0; i < passwordLength; i++) {
+        result += LowercaseUppercaseSpecial[Math.floor(Math.random() * LowercaseUppercaseSpecial.length)];
+      }
+    } else if (includeLowercase === true && includeUppercase === false && includeNumeric === true && includeSpecial === true) {
+      for (var i = 0; i < passwordLength; i++) {
+        result += LowercaseNumericSpecial[Math.floor(Math.random() * LowercaseNumericSpecial.length)];
+      }
+    } else if (includeLowercase === true && includeUppercase === true && includeNumeric === true && includeSpecial === true) {
+      for (var i = 0; i < passwordLength; i++) {
+        result += LowercaseUppercaseNumericSpecial[Math.floor(Math.random() * LowercaseUppercaseNumericSpecial.length)];
+      }
+    } else if (includeLowercase === false && includeUppercase === true && includeNumeric === false && includeSpecial === false) {
+      for (var i = 0; i < passwordLength; i++) {
+        result += Uppercase[Math.floor(Math.random() * Uppercase.length)];
+      }
+    } else if (includeLowercase === false && includeUppercase === true && includeNumeric === true && includeSpecial === false) {
+      for (var i = 0; i < passwordLength; i++) {
+        result += UppercaseNumeric[Math.floor(Math.random() * UppercaseNumeric.length)];
+      }
+    } else if (includeLowercase === false && includeUppercase === true && includeNumeric === false && includeSpecial === true) {
+      for (var i = 0; i < passwordLength; i++) {
+        result += UppercaseSpecial[Math.floor(Math.random() * UppercaseSpecial.length)];
+      }
+    } else if (includeLowercase === false && includeUppercase === true && includeNumeric === true && includeSpecial === true) {
+      for (var i = 0; i < passwordLength; i++) {
+        result += UppercaseNumericSpecial[Math.floor(Math.random() * UppercaseNumericSpecial.length)];
+      }
+    } else if (includeLowercase === false && includeUppercase === false && includeNumeric === true && includeSpecial === false) {
+      for (var i = 0; i < passwordLength; i++) {
+        result += Numeric[Math.floor(Math.random() * Numeric.length)];
+      }
+    } else if (includeLowercase === false && includeUppercase === false && includeNumeric === true && includeSpecial === true) {
+      for (var i = 0; i < passwordLength; i++) {
+        result += NumericSpecial[Math.floor(Math.random() * NumericSpecial.length)];
+      }
+    } else if (includeLowercase === false && includeUppercase === false && includeNumeric === false && includeSpecial === true) {
+      for (var i = 0; i < passwordLength; i++) {
+        result += Special[Math.floor(Math.random() * Special.length)];
+      }
+    } else {
+      alert("Please Enter Valid Criteria")
+    }
+  }
+  // calling the function directly above
+  generatePassword()
+  // printing the result to the page
+  var passwordText = document.querySelector("#password");
+  passwordText.value = result;
 }
-console.log("-------------------------------------------------")
-// code to pull a random element from an array
-console.log(Lowercase[Math.floor(Math.random() * Lowercase.length)]);
-console.log(Uppercase[Math.floor(Math.random() * Uppercase.length)]);
-console.log(Numeric[Math.floor(Math.random() * Numeric.length)]);
-console.log(Special[Math.floor(Math.random() * Special.length)]);
-console.log("-------------------------------------------------")
-// code to pull random element for set number of characters
-var characters = 10;
-for (var i = 0; i < characters; i++) {
-  console.log(Lowercase[Math.floor(Math.random() * Lowercase.length)]);
-}
-
-
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// need to research "validated" and "character type"
-
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// cross-reference generated password w/ criteria selected earlier. use a function here?
-
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-// alert("Password is: ....");
-// Add event listener to generate button
-// // TODO come back to this, uncomment this line and the one below, tie to button click
-// generateBtn.addEventListener("click", writePassword);
